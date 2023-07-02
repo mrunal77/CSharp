2300297b54a5c644921f09c99fa65c7bb7aea4d9
mrunal77 <mtmeshram77@outlook.com>
2023-07-02 16:10:09 +0530

    API Test with dotnet  and mongo db -- Initial commit

---
+++ Hello/WebAPI_Test/Model/Book.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebAPI_Test.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [JsonIgnore]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string BookName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
